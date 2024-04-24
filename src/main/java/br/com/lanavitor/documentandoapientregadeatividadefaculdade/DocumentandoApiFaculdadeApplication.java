7a8472ed725c76b55b4cb95f3fc35d7291345931
vitorlana45 <vitoresmerio00@gmail.com>
2024-04-24 14:48:55 -0300

    initial Commit

---
+++ src/main/java/br/com/lanavitor/documentandoapientregadeatividadefaculdade/DocumentandoApiFaculdadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.lanavitor.documentandoapientregadeatividadefaculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentandoApiFaculdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentandoApiFaculdadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lanavitor/documentandoapientregadeatividadefaculdade/DocumentandoApiFaculdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lanavitor.documentandoapientregadeatividadefaculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentandoApiFaculdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
